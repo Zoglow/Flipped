89e2f2ddbf65ba414310737fbca982503687e312
Zoe Olson <zoglow2311@gmail.com>
2023-09-25 13:38:31 -0400

    Initial Commit

---
+++ Flipped/FlippedApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlippedApp.swift
+//  Flipped
+//
+//  Created by Zoe Olson on 9/25/23.
+//
+
+import SwiftUI
+
+@main
+struct FlippedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
