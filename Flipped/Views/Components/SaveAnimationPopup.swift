9131c1eaaafae78a55c19b560b0f78fa932acfd0
Zoe Sosa <zoglow2311@gmail.com>
2024-03-21 12:21:19 -0400

    add popup for saving gifs

---
+++ Flipped/Views/Components/SaveAnimationPopup.swift
@@ -0,0 +1,18 @@
+//
+//  SaveAnimationPopup.swift
+//  Flipped
+//
+//  Created by Zoe Olson on 3/21/24.
+//
+
+import SwiftUI
+
+struct SaveAnimationPopup: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SaveAnimationPopup()
+}
