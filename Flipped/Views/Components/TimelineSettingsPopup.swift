a0a5fec8be26cab01945620ff4f6c14d0875bd58
Zoe Sosa <zoglow2311@gmail.com>
2024-03-22 20:50:36 -0400

    Menu for timeline finished + frame rate is now changeable

---
+++ Flipped/Views/Components/TimelineSettingsPopup.swift
@@ -0,0 +1,18 @@
+//
+//  TimelineSettingsPopup.swift
+//  Flipped
+//
+//  Created by Zoe Olson on 3/21/24.
+//
+
+import SwiftUI
+
+struct TimelineSettingsPopup: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimelineSettingsPopup()
+}
